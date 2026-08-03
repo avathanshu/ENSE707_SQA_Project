1e4da9c0bbc52677189be0bb78a49b7fc2d1211a
Quentin Masoe <masoe.quentin@gmail.com>
2026-08-04 04:56:12 +1200

    added gitIgnore and sotre sim logic with ai genereated test for main constraints

---
+++ StoreSim/StoreSim/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreSim
+{
+    public class Customer
+    {
+        private static int _idCounter = 1;
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        
+        public Customer(String name)
+        {
+            Id = _idCounter++;
+            Name = name;
+        }
+    }
+}
